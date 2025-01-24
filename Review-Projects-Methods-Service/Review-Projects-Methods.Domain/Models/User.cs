b81f6d0e36c33e9f0ce8c7927abfbd435e07a561
fabinho1992 <f.santosdev1992@gmail.com>
2025-01-24 16:55:16 -0300

    Create Users

--- Review-Projects-Methods-Service/Review-Projects-Methods.Domain/Models/User.cs
+++ Review-Projects-Methods-Service/Review-Projects-Methods.Domain/Models/User.cs
@@ -8,6 +8,10 @@ namespace Review_Projects_Methods.Domain.Models
 {
     public class User
     {
-
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public DateTime CreateAt { get; set; }
+        public TypeUserEnum TypeUser { get; set; }
     }
 }
